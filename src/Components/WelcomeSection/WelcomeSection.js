51679f066bf02398c2ec4e73a0f820351fc26129
Mikolaj Adam Manczak <mikomanczak@gmail.com>
2018-10-06 11:32:21 +0200

    updated link to cv

--- src/Components/WelcomeSection/WelcomeSection.js
+++ src/Components/WelcomeSection/WelcomeSection.js
@@ -25,7 +25,7 @@ const WelcomeSection = () => {
                     <a href="https://github.com/mikomanczak" className="github" target="_blank">
                         <img src={githubIcon} className="profile-icon" alt="gitHub icon" />
                     </a>
-                    <a href="https://raw.githubusercontent.com/mikomanczak/Portfolio/5bd76bbddf1705943b026f35a04660031d69661a/CV.pdf" className="cv" target="_blank">
+                    <a href="https://raw.githubusercontent.com/mikomanczak/Portfolio/e6ffb5f5d7fca47cee6b70f79ff85a70b65c06f3/CV.pdf" className="cv" target="_blank">
                         <img src={cvIcon} className="profile-icon" alt="CV icon" />
                     </a>
                 </div>
